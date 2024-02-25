769e9109acb9fbb5c9e19b08ff16322cf2604a45
Wes Talley <wesatalley@gmail.com>
2024-02-25 14:11:23 -0600

    Added nodemon tool / basics of backend

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 const express = require('express')
 const app = express()
+
+class User {
+    constructor (email, password, firstname, lastname) {
+        this._email = email
+        this._password = password
+        this._firstname = firstname
+        this._lastname = lastname
+    }
+
+    getPassword() { return this._password } 
+    getEmail() { return this._email }
+    getFullName() { return this._firstname + ' ' + this._lastname }
+}
+
+app.get('/api/users', (req, res) => {
+    
+})
+
+app.listen(6969, () => {
+    console.log('Listening on port 6969, hehe...')
+})
