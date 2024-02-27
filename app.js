3a1b531366704c14b967f4d9e8a3dd849323c594
Wes Talley <wesatalley@gmail.com>
2024-02-26 23:03:54 -0600

    Added MariaDB / First steps of full-stack foundations

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 class User {
     constructor (firstname, lastname, email, password, sessionID) {
-        this.Firstname = firstname
-        this.Lastname = lastname
+        this.FirstName = firstname
+        this.LastName = lastname
         this.Email = email
         this.Password = password
         this.SessionID = sessionID
@@ -15,17 +15,55 @@ class User {
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mariadb = require('mariadb')
 const app = express()
 
 app.use(bodyParser.json())
 
 app.use(cors())
 
+const pool = mariadb.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Mickey2024!',
+    database: 'users_db',
+    connectionLimit: 5
+})
+
+async function createUserTable() {
+    let conn;
+    try {
+        conn = await pool.getConnection()
+        const table = await conn.query('CREATE table USERS(FirstName varchar(50) primary key, LastName varchar(50), Email varchar(100), Password varchar(50), SessionID varchar(10));')
+        console.log(table)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        if(conn) conn.release()
+    }
+}
+
+async function addUser(newUser) {
+    let conn;
+    try {
+        conn = await pool.getConnection()
+        const user = await conn.query(`INSERT into USERS values ('${newUser.FirstName}', '${newUser.LastName}', '${newUser.Email}', '${newUser.Password}', '${newUser.SessionID}');`)
+        console.log(user)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        if(conn) conn.release()
+    }
+}
+
 app.post('/api/users', (req, res) => {
     let { FirstName, LastName, Email, Password } = req.body
     let SessionID = '123-456'
     let newUser = new User(FirstName, LastName, Email, Password, SessionID)
     // send to database yada yada
+    addUser(newUser)
+
     res.status(201).json(newUser)
 })
 
@@ -33,6 +71,6 @@ app.post('/api/sessions', (req, res) => {
     
 })
 
-app.listen(6969, () => {
-    console.log('Listening on port 6969, hehe...')
+app.listen(3000, () => {
+    console.log('Listening on port 3000...')
 })
