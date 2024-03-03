64a7c7033a9201e83342ebb27dadafaf3d8f6975
Wes Talley <wesatalley@gmail.com>
2024-03-02 18:36:13 -0600

    Sessions ALMOST finished; need to find out how to auto-delete session on close.

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ class User {
         this.LastName = lastname
         this.Email = email
         this.Password = password
+        this.SessionID = sessionID
     }
 
     getPassword() { return this.Password } 
     getEmail() { return this.Email }
     getFullName() { return this.FirstName + ' ' + this.LastName }
+}
+
+class Session {
+    constructor(sessionID, idCreateDate) {
+        this.SessionID = sessionID
+        this.IDCreateDate = idCreateDate
+    }
 }
 
 const express = require('express')
@@ -37,15 +45,16 @@ const userPool = mariadb.createPool({
     port: 3306,
     user: 'root',
     password: 'Mickey2024!',
-    database: 'users_db',
+    database: 'mimic_db',
     connectionLimit: 5
 })
 
-async function createUserTable() {
+// function to automatically create users table for me more efficiently.
+async function createUsersTable() {
     let conn;
     try {
         conn = await userPool.getConnection()
-        const table = await conn.query('create table USERS(FirstName varchar(50) not null, LastName varchar(50) not null, Email varchar(100) primary key, Password varchar(50) not null);')
+        const userTable = await conn.query('create table users (FirstName varchar(50) not null, LastName varchar(50) not null, Email varchar(100) primary key, Password varchar(50) not null);')
     } catch (error) {
         console.log(error)
     } finally {
@@ -53,11 +62,26 @@ async function createUserTable() {
     }
 }
 
+// function to automatically create sessions table for me more efficiently.
+async function createSessionsTable() {
+    let conn
+    try {
+        conn = await userPool.getConnection()
+        const sessTable = await conn.query('create table sessions (SessionID char(36) primary key, Email varchar(100) not null, IDCreateDate varchar(50) not null, foreign key(Email) references users(Email));')
+    } catch (error) {
+        console.log(error)
+    } finally {
+        if(conn) conn.release()
+    }
+}
+
+// this returns truthy response from MariaDB to the register POST below.
 async function addUser(newUser) {
-    let conn;
+    let conn
     try {
         conn = await userPool.getConnection()
-        return await conn.query(`insert into USERS values ('${newUser.FirstName}', '${newUser.LastName}', '${newUser.Email}', '${newUser.Password}');`)
+        return await conn.query(`insert into users values (?, ?, ?, ?);`, 
+        [`${newUser.FirstName}`, `${newUser.LastName}`, `${newUser.Email}`, `${newUser.Password}`])
     } catch (error) {
         console.log(error)
     } finally {
@@ -65,24 +89,46 @@ async function addUser(newUser) {
     }
 }
 
-//createUserTable()
+//createUsersTable()
+//createSessionsTable()
 
-// Partially done validating user credentials for logging in.
+// Returns the rows from MariaDB where password matches the username, aka logging in.
 async function getUser(Email, Password) {
-    let conn;
+    let conn
     try {
         conn = await userPool.getConnection()
-        return await conn.query(`select * from USERS where Email = '${Email}' and Password = '${Password}';` /*[Email, Password]*/)
+        return await conn.query(`select * from users where Email = ? and Password = ?;`, [`${Email}`, `${Password}`])
     } catch (error) {
         console.log(error)
     } finally {
         if(conn) conn.release()
+    }
+}
+
+// adds a new session to the sessions table.
+async function addSession(SessionID, Email, IDCreateDate) {
+    let conn
+    try {
+        conn = await userPool.getConnection()
+        return await conn.query(`insert into sessions values (?, ?, ?);`, [`${SessionID}`, `${Email}`, `${IDCreateDate}`])
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function removeSession(SessionID) {
+    let conn
+    try {
+        conn = await userPool.getConnection()
+        return await conn.query(`delete from sessions where SessionID = ?;`, [`${SessionID}`])
+    } catch (error) {
+        console.log(error)
     }
 }
 
 // This POST takes care of registering an account.
 app.post('/api/users', async (req, res) => {
-    try{
+    try {
         let { FirstName, LastName, Email, Password } = req.body
         let newUser = new User(FirstName, LastName, Email, Password)
         // send to database yada yada
@@ -103,9 +149,17 @@ app.get('/api/users', async (req, res) => {
         const rows = await getUser(Email, Password)
         if(rows.length > 0)
         {
-            const SessionID = req.sessionID // generates a new SessionID for our user every login.
+            const SessionID = req.sessionID // generates a new SessionID for our user every time login button is used correctly.
             const user = rows[0]
             user.SessionID = SessionID
+            // This is how Chat told me to get the date in a nice YYYY-MM-DD format :)
+            const currentDate = new Date(Date.now())
+            const year = currentDate.getFullYear()
+            const month = String(currentDate.getMonth() + 1).padStart(2, '0')
+            const day = String(currentDate.getDate()).padStart(2, '0')
+            //
+            const IDCreateDate = `${year}-${month}-${day}`
+            const session = await addSession(SessionID, Email, IDCreateDate)
             res.status(200).json(user)
         }
         else
@@ -114,6 +168,11 @@ app.get('/api/users', async (req, res) => {
     } catch (error) {
         console.log(error)
     }
+})
+//!!! TALK TO BURCHFIELD ABOUT HOW TO BETTER HANDLE ALL OF THESE ENDPOINTS TO MATCH UP WITH THEIR DATABASE TABLES!
+app.delete('/api/users', async (req, res) => {
+    let { SessionID } = req.query
+    const response = await removeSession(SessionID)
 })
 
 const port = process.env.port || 3000;
